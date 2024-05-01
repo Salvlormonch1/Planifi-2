896630bcff128eb7c202d3522a7d9fc37e15f27c
Rodrigo2901 <u202215489@upc.edu.pe>
2024-04-30 20:26:49 -0500

    feat: added services for uses

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './app.vue'
+import router from "./router/index.js";
+import PrimeVue from "primevue/config";
+import ConfirmationService from "primevue/confirmationservice";
+import DialogService from "primevue/dialogservice";
+import ToastService from "primevue/toastservice";
+import {PrimeIcons} from "primevue/api";
+import 'primeicons/primeicons.css'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(router)
+    .use(PrimeVue, {ripple: true})
+    .use(ConfirmationService)
+    .use(DialogService)
+    .use(ToastService)
+    .use(PrimeIcons)
+    .mount('#app')
