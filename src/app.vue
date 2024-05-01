9387594ac8ed74d44f49e574c736d2dbffb42b33
Rodrigo2901 <u202215489@upc.edu.pe>
2024-04-30 20:26:34 -0500

    feat: added script, template and services for uses.

--- src/app.vue
+++ src/app.vue
@@ -1,7 +1,23 @@
 <script>
+export default {
+  name: 'app',
+  title: 'Planifi',
+  data() {
+    return {
+      drawer: false,
+      items: []
+    }
+  },
+  methods: {
+    toggleDrawer() {
+      this.drawer = !this.drawer
+    }
+  }
+}
 </script>
 
 <template>
+  <router-view/>
 </template>
 
 <style>
