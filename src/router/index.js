ab4c8c3a60946bc663697a77bc4d91917b08cb29
Rodrigo2901 <u202215489@upc.edu.pe>
2024-04-30 20:51:46 -0500

    feat(sign-in-up): added login and register components in index and app.

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,14 @@
 import {createRouter, createWebHistory} from "vue-router";
 import CheckAccountComponent from "../public/pages/check-account.component.vue";
+import loginComponent from "../iam/pages/login.component.vue";
+import registerComponent from "../iam/pages/register.component.vue";
 
 const router = createRouter( {
     history: createWebHistory(),
     routes: [
         { path: '/check-account', component: CheckAccountComponent, meta: {title: 'Check Account'}},
+        { path: '/login', component: loginComponent, meta: {title: 'Login'}},
+        { path: '/register', component: registerComponent, meta: {title: 'Register'}},
         { path: '/', redirect: '/check-account'}
     ]
 })
